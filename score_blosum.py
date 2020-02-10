f2e3adbe43a09b3cdb408441d5bb4650a49c902b
mlaserra <59024742+mlaserra@users.noreply.github.com>
2020-02-10 23:20:34 +0100

    Update score_blosum.py

--- score_blosum.py
+++ score_blosum.py
@@ -1,22 +1,20 @@
 seq1 ="ALASVLIRLITRLYP"
 seq2 ="ASAVHLNRLITRLYP"
 def score(seq1,seq2):
-    blosum = {}
-    blosum_file = open("blosum.txt","r")
-#Create a list of AA:
-#Take the first line
-    AA = blosum_file.readline()
-#Create a list of the characters of the sequences
-    AA = AA.split()
+    blosum = {}                                     #Create an empty dictionary for the pairs of the matrix
+    blosum_file = open("blosum.txt","r")            # Read the matrix
+    aminoacids = blosum_file.readline()             
+    aminoacids = AA.split()                         #Create a list of the aminoacids of the matrix
     for line in blosum_file:
-        string = line.split()
-        for i in range(len(AA)):
-            blosum[AA[i]+string[0]] = string[i+1]
+        values = line.split()                       #Iterate for each line creating a list of the values associated to each pairs of AA
+        for i in range(len(aminoacids)):                        #Iterate for all the AA filling the blosum dictionary
+            blosum[aminoacids[i]+values[0]] = values[i+1]       
     score = 0
     for i in range(len(seq1)):
-        score = score + int(blosum[seq1[i]+seq2[i]])
-    return score
-    blosum_file.close()
+        if len(seq1)== len(seq2):
+            score = score + int(blosum[seq1[i]+seq2[i]])            #Calculate the score for the aligned residues using the values
+    return score                                                    #from blosum dictionary
+    blosum_file.close()                                             #Close the file
 
 print "The score of the sequences alignment with BLOSUM matrix is", score(seq1,seq2)
 
