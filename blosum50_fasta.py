1c0ca23accde463c0383d7b2b67bb4c7cf0691f8
mlaserra <59024742+mlaserra@users.noreply.github.com>
2020-02-11 11:01:19 +0100

    Update blosum50_fasta.py

--- blosum50_fasta.py
+++ blosum50_fasta.py
@@ -3,10 +3,13 @@ def blosum50_score(L):
                 'AG': 0, 'AH':-2, 'AI':-1, 'AL':-2, 'AK':-1, 'AM':-1,'AF':-3,
                 'AP':-1, 'AS': 1, 'AT': 0, 'AW':-3, 'AY':-2, 'AV': 0}
     score = 0
-    for i in range(len(L[0])):
-        if len(L[0])==len(L[1]):
-            if L[0][i]+L[1][i] in blosum50.keys():
-                score = score + int(blosum50[L[0][i]+L[1][i]])
+    if len(L) != 2:
+        print "Error! More that two sequences"
+     else:
+        for i in range(len(L[0])):
+            if len(L[0])==len(L[1]):
+                if L[0][i]+L[1][i] in blosum50.keys():
+                    score = score + int(blosum50[L[0][i]+L[1][i]])
     return score
 
 def take_seq(fasta_file):
