f8201bc89776a793ccdec2714e58a656333178fb
mlaserra <59024742+mlaserra@users.noreply.github.com>
2020-02-12 17:28:38 +0100

    Create read_square_matrix.py

---
+++ read_square_matrix.py
@@ -0,0 +1,12 @@
+def read_matrix(matrix_file):
+    matrix = {}
+    infile = open(matrix_file, "r")
+    AA = infile.readline().split()
+    for line in infile:
+        line = line.split()
+        for i in range(len(AA)):
+            matrix[AA[i]+line[0]]= int(line[i+1])
+            matrix[line[0]+AA[i]]= int(line[i+1])
+    return matrix
+
+print read_matrix("blosum.txt")
