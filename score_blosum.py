959c9a643cd00998233f95534b4eab55f20ee420
mlaserra <59024742+mlaserra@users.noreply.github.com>
2020-02-12 16:03:23 +0100

    Update score_blosum.py

--- score_blosum.py
+++ score_blosum.py
@@ -5,9 +5,9 @@ def score(seq1,seq2):
     blosum_file = open("blosum.txt","r")            # Read the matrix
     aminoacids = blosum_file.readline().split()     #Create a list of the aminoacids of the matrix
     for line in blosum_file:
-        values = line.split()                       #Iterate for each line creating a list of the values associated to each pairs of AA
+        line = line.split()                       #Iterate for each line creating a list of the values associated to each pairs of AA
         for i in range(len(aminoacids)):                        #Iterate for all the AA filling the blosum dictionary
-            blosum[aminoacids[i]+values[0]] = values[i+1]       
+            blosum[aminoacids[i]+line[0]] = line[i+1]       
     score = 0
     for i in range(len(seq1)):
         if len(seq1)== len(seq2):
